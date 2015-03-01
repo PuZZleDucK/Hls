6cfdd37be3398fdca51ee34914c01d14b01faacf
PuZZleDucK <puzzleduck@gmail.com>
2015-03-01 23:10:15 +1100

    Shell of Hecho working.

--- Hecho.hs
+++ Hecho.hs
@@ -1,4 +1,4 @@
--- H<app-name>, a haskell implementation of GNU <app-name>.
+-- Hecho, a haskell implementation of GNU echo.
 module Main where
 import System.Environment
 import System.Console.Terminfo.Base
@@ -18,20 +18,20 @@ main = do
   runTermOutput term (termText (output))
   return ()
 
-showOutput :: <app-name>Options -> IO String
+showOutput :: EchoOptions -> IO String
 showOutput opts | not ((displayHelp opts) || (displayVersion opts)) = return "" -- <do-stuff-Here>
                 | otherwise = return ""
 
 
-showHelp :: <app-name>Options -> String
+showHelp :: EchoOptions -> String
 showHelp opts | (displayHelp opts) = concat (intersperse "\n" helpText)
               | otherwise = ""
 
-showVersion :: <app-name>Options -> String
+showVersion :: EchoOptions -> String
 showVersion opts | (displayVersion opts) = concat (intersperse "\n" versionText)
                  | otherwise = ""
 
-processArgs :: [String] -> <app-name>Options -> <app-name>Options
+processArgs :: [String] -> EchoOptions -> EchoOptions
 processArgs [] opts = opts
 processArgs (x:xs) opts = case x of
   "--help" -> processArgs xs opts{displayHelp = True}
@@ -42,22 +42,52 @@ stripQuotes :: String -> String
 stripQuotes ('"':xs) = if last xs == '"' then init xs else ('"':xs)
 stripQuotes xs = xs
 
-defaultOptions :: <app-name>Options
-defaultOptions = <app-name>Options False False
+defaultOptions :: EchoOptions
+defaultOptions = EchoOptions False False
 
-data <app-name>Options = <app-name>Options
+data EchoOptions = EchoOptions
   { displayHelp :: Bool
   , displayVersion :: Bool } deriving (Show, Eq)
 
 
 helpText :: [String]
-helpText = [ ""
-           , "keep the newline->\n"
+helpText = [ "Usage: ./src/echo [SHORT-OPTION]... [STRING]..."
+           , "  or:  ./src/echo LONG-OPTION"
+           , "Echo the STRING(s) to standard output."
+           , "  -n do not output the trailing newline"
+           , "  -e             enable interpretation of backslash escapes"
+           , "  -E             disable interpretation of backslash escapes (default)"
+           , "      --help display this help and exit"
+           , "      --version output version information and exit"
+           , "If -e is in effect, the following sequences are recognised:"
+           , "  \\\\      backslash"
+           , "  \\a      alert (BEL)"
+           , "  \\b      backspace"
+           , "  \\c      produce no further output"
+           , "  \\e      escape"
+           , "  \\f      form feed"
+           , "  \\n      new line"
+           , "  \\r      carriage return"
+           , "  \\t      horizontal tab"
+           , "  \\v      vertical tab"
+           , "  \\0NNN   byte with octal value NNN (1 to 3 digits)"
+           , "  \\xHH    byte with hexadecimal value HH (1 to 2 digits)"
+           , "NOTE: your shell may have its own version of echo, which usually supersedes"
+           , "the version described here. Please refer to your shell's documentation"
+           , "for details about the options it supports."
+           , "GNU coreutils online help: <http://www.gnu.org/software/coreutils/>"
+           , "Full documentation at: <http://www.gnu.org/software/coreutils/echo>"
+           , "or available locally via: info '(coreutils) echo invocation'\n"
            ]
 
 versionText :: [String]
 versionText = [ "H<app-name> (Haskell implementation of GNU <app-name>) 1.0"
-              , "derrived from: "
+              , "derrived from: echo (GNU coreutils) 8.23.138-7ceaf"
+              , "Copyright (C) 2015 Free Software Foundation, Inc."
+              , "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>."
+              , "This is free software: you are free to change and redistribute it."
+              , "There is NO WARRANTY, to the extent permitted by law."
+              , "Written by Brian Fox and Chet Ramey."
               , "Ported by PuZZleDucK.\n"
               ]
 
