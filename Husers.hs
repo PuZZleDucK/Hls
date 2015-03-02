79502139d4b85dd1048dadb7913b30771de44c60
PuZZleDucK <puzzleduck@gmail.com>
2015-03-03 10:46:53 +1100

    Uers text entered

--- Husers.hs
+++ Husers.hs
@@ -14,6 +14,7 @@ main = do
 
   runTermOutput term (termText (showHelp options))
   runTermOutput term (termText (showVersion options))
+  --needs to possibly process a file instead of querying system.
   output <- showOutput options
   runTermOutput term (termText (output))
   return ()
@@ -51,13 +52,25 @@ data UsersOptions = UsersOptions
 
 
 helpText :: [String]
-helpText = [ ""
-           , "keep the newline->\n"
+helpText = [ "Usage: users [OPTION]... [FILE]"
+           , "Output who is currently logged in according to FILE."
+           , "If FILE is not specified, use /var/run/utmp.  /var/log/wtmp as FILE is common."
+           , "      --help     display this help and exit"
+           , "      --version  output version information and exit"
+           , "Report users bugs to bug-coreutils@gnu.org"
+           , "GNU coreutils home page: <http://www.gnu.org/software/coreutils/>"
+           , "General help using GNU software: <http://www.gnu.org/gethelp/>"
+           , "For complete documentation, run: info coreutils 'users invocation'\n"
            ]
 
 versionText :: [String]
 versionText = [ "H<app-name> (Haskell implementation of GNU <app-name>) 1.0"
-              , "derrived from: "
+              , "derrived from: users (GNU coreutils) 8.13"
+              , "Copyright (C) 2011 Free Software Foundation, Inc."
+              , "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>."
+              , "This is free software: you are free to change and redistribute it."
+              , "There is NO WARRANTY, to the extent permitted by law."
+              , "Written by Joseph Arceneaux and David MacKenzie."
               , "Ported by PuZZleDucK.\n"
               ]
 
