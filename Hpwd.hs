8c57e421131a18e3048073c85464dbded77c0c98
Ben Minerds <puzzleduck@gmail.com>
2015-03-19 03:12:53 +1100

    ... and when I say Hecho, I mean Hwpd

--- Hpwd.hs
+++ Hpwd.hs
@@ -1,20 +1,22 @@
 -- Hpwd, a haskell implementation of GNU pwd.
+{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances #-}
 module Main where
 import System.Environment
 import System.Console.Terminfo.Base
 import Data.List
+import GnUtils
 
 main :: IO ()
 main = do
   term <- setupTermFromEnv
   args <- getArgs
-  let options = processArgs args defaultPwd
-  runTermOutput term (termText ("Options: "++(show options)++"\n"))
+  let options = processArgs pwdFlags args defaultPwd
 
   runTermOutput term (termText (showHelp options))
   runTermOutput term (termText (showVersion options))
   output <- showOutput options
   runTermOutput term (termText (output))
+  runTermOutput term (termText ("Options: "++(stripQuotes (show options))++"\n"))
   return ()
 
 showOutput :: PwdOptions -> IO String
@@ -30,26 +32,71 @@ showVersion :: PwdOptions -> String
 showVersion opts | (displayVersion opts) = concat (intersperse "\n" versionText)
                  | otherwise = ""
 
-processArgs :: [String] -> PwdOptions -> PwdOptions
-processArgs [] opts = opts
-processArgs (x:xs) opts = case x of
-  "--help" -> processArgs xs opts{displayHelp = True}
-  "--version" -> processArgs xs opts{displayVersion = True}
-  "--" -> opts --GNU extension, terminate option parsing, still need to parse targets
-  _ -> processArgs xs opts -- if POSIXLY_CORRECT: processTargets xs opts
-                           -- as POSIX terminates option processing after the first target
+--data ProcessingState = Normal | LongOpt | ShortOpt | NoOpts
 
-stripQuotes :: String -> String
-stripQuotes ('"':xs) = if last xs == '"' then init xs else ('"':xs)
-stripQuotes xs = xs
+
+
+
+
+
+
+
+
+--processArgs (x:xs) opts = case x of
+--  "--help" -> processArgs xs opts{displayHelp = True}
+--  "--version" -> processArgs xs opts{displayVersion = True}
+--  "--" -> opts --GNU extension, terminate option parsing, still need to parse targets
+--  _ -> processArgs xs opts -- if POSIXLY_CORRECT: processTargets xs opts
+--                           -- as POSIX terminates option processing after the first target
+--processArgs (x:xs) opts = if thisOption == "" then processArgs xs opts -- no targets
+--                                              else (effect (getOptionFlag x)) opts
+--  where thisOption = getOption x
+
+--getOption :: String -> String -- "" means target/not-option
+--getOption [] = ""
+--getOption (x:[]) = ""
+--getOption (x:y:[]) = ""
+--getOption str = if head str /= '-' then ""
+--                                   else if head ((drop 1) str) == '-' then (drop 2) str
+--                                                                      else (drop 1) str
+
+--getOptionFlag :: String -> OptionFlags
+--getOptionFlag (ch:[]) = head (filter (\x -> (shortTag x == ch)) pwdFlags)
+--getOptionFlag str = head (filter (\x -> (longTag x == str)) pwdFlags)
+
 
 defaultPwd :: PwdOptions
-defaultPwd = PwdOptions False False
+defaultPwd = PwdOptions False False True []
+
+
+--newtype DefaultOptions = PwdOptions
+--  help :: a
+--  others :: a
 
 data PwdOptions = PwdOptions
   { displayHelp :: Bool
-  , displayVersion :: Bool } deriving (Show, Eq)
+  , displayVersion :: Bool
+  , resolveSymlinks :: Bool
+  , targets :: [String] } deriving (Show, Eq)
 
+pwdFlags :: [OptionFlags PwdOptions]
+pwdFlags = 
+  [ (OF ""
+        '\0' ""
+        (\x -> x{targets = (targets x)}))
+  , (OF "display this help and exit"
+        '\0' "help"
+        (\x -> x{displayHelp = True}))
+  , (OF "output version information and exit"
+        '\0' "version"
+        (\x -> x{displayVersion = True}))
+  , (OF "use PWD from environment, even if it contains symlinks"
+        'L' "logical"
+        (\x -> x{resolveSymlinks = False}))
+  , (OF "avoid all symlinks"
+        'P' "physical"
+        (\x -> x{resolveSymlinks = True}))
+  ]
 
 helpText :: [String]
 helpText = [ "Usage: /home/bminerds/x/coreutils/src/pwd [OPTION]..."
